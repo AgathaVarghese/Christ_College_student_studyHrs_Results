a212c13131fc9350a1518aad94aa38ec09923375
AgathaVarghese <150008124+AgathaVarghese@users.noreply.github.com>
2026-09-17 12:10:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 from pathlib import Path
 
-model_path = Path(_file_).parent / "student_pass_fail_modell.pkl"
+model_path = Path(__file__).parent / "student_pass_fail_modell.pkl"
 model = joblib.load(model_path)
 
 st.title("Student Pass Predictor")
